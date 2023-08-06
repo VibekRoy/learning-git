929eed24513e746400a94a0ae2d2df408bc285a2
Vibek Roy <vibekroy321@gmail.com>
2023-08-06 07:24:19 +0530

    'v1.0'

---
+++ pairs.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   pair <int,int> p = {1,2};
+   cout<< p.first<<endl;
+
+    pair <int,pair<int,int>> q = {1,{2,3}};
+    cout<<q.second.first;
+   return 0;
+}
